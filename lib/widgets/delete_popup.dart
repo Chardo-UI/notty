6a6f1eed498c460f66f600b8c78b903ef958b025
Chardo-UI <charlinainggolan525@gmail.com>
2022-01-19 22:49:22 +0700

    first commit

---
+++ lib/widgets/delete_popup.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notes/helper/note_provider.dart';
+import 'package:flutter_notes/models/note.dart';
+import 'package:provider/provider.dart';
+
+class DeletePopUp extends StatelessWidget {
+  final Note selectedNote;
+
+  DeletePopUp(this.selectedNote);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+      title: Text('Hapus?'),
+      content: Text('Apakah anda yakin menghapus?'),
+      actions: [
+        TextButton(
+          child: Text('Ya'),
+          onPressed: () {
+            Provider.of<NoteProvider>(context, listen: false)
+                .deleteNote(selectedNote.id);
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+          },
+        ),
+        TextButton(
+          child: Text('Tidak'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  }
+}
